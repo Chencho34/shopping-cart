020c1882c8369b03b5f48c1ba4a9c6e619077ea3
Armando <armandocrescencio343@gmail.com>
2024-03-29 22:23:59 -0600

    SET: Eslint config

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,35 +1,36 @@
-import { Link, NavLink, useLocation } from "react-router-dom";
-import { FaCartShopping } from "react-icons/fa6";
-import { FaStore } from "react-icons/fa6";
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import { FaCartShopping } from 'react-icons/fa6'
+import { FaStore } from 'react-icons/fa6'
 
 interface PathsTypes {
-  id: number;
-  item: string;
-  to: string;
+  id: number
+  item: string
+  to: string
   icon: JSX.Element
 }
 
-export default function Nav() {
-  const location = useLocation();
-  const paths = [
-    { id: 1, item: "Products", to: "/", icon: <FaStore /> },
-    { id: 2, item: "Cart", to: "/cart", icon: <FaCartShopping /> },
-  ];
+export default function Nav () {
+  const location = useLocation()
+  const paths: PathsTypes[] = [
+    { id: 1, item: 'Products', to: '/', icon: <FaStore /> },
+    { id: 2, item: 'Cart', to: '/cart', icon: <FaCartShopping /> }
+  ]
 
   return (
-    <nav className="h-20 w-full bg-[#111111]">
-      <section className="h-full w-[80%] mx-auto flex items-center justify-between">
-        <article className="text-gray-400 flex items-center gap-3 space-y-2">
-          <Link to="/" className="text-2xl">
+    <nav className='h-20 w-full bg-[#111111]'>
+      <section className='h-full w-[80%] mx-auto flex items-center justify-between'>
+        <article className='text-gray-400 flex items-center gap-3 space-y-2'>
+          <Link to='/' className='text-2xl'>
             Shopping-Cart
           </Link>
-          <p className="">E-comerce</p>
+          <p className=''>E-comerce</p>
         </article>
-        <ul className="flex gap-6 text-gray-400">
-          {paths.map(({ icon, id, item, to }: PathsTypes) => (
+        <ul className='flex gap-6 text-gray-400'>
+          {paths.map(({ icon, id, item, to }) => (
             <li key={id}>
               <NavLink
-                className={`flex items-center gap-2 ${location.pathname === to ? 'text-orange-400' : ''}`}
+                className={`flex items-center gap-2 ${location.pathname === to ? 'text-orange-400' : ''
+                  }`}
                 to={to}
               >
                 {icon}
@@ -40,5 +41,5 @@ export default function Nav() {
         </ul>
       </section>
     </nav>
-  );
+  )
 }
