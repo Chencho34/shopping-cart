04691ae5f34ff1e8ca76cc2b01eb1241bc75bb52
Jose Armando CR <90371472+Chencho34@users.noreply.github.com>
2024-03-29 22:41:14 -0600

    ADD: NavBar Component (#1)
    * WIP: Setting styles and routing to Nav
    
    * SET: Eslint config
    
    * ADD: wrapper and container component
    
    * import components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 import Card from './card/Card'
 import Footer from './footer/Footer'
 import Nav from './nav/Nav'
+import Container from './container/Container'
+import Wrapper from './wrapper/Wrapper'
 
 export {
   Card,
   Footer,
-  Nav
+  Nav,
+  Container,
+  Wrapper
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,8 +1,45 @@
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import { FaCartShopping } from 'react-icons/fa6'
+import { FaStore } from 'react-icons/fa6'
 
-export default function Nav() {
+interface PathsTypes {
+  id: number
+  item: string
+  to: string
+  icon: JSX.Element
+}
+
+export default function Nav () {
+  const location = useLocation()
+  const paths: PathsTypes[] = [
+    { id: 1, item: 'Products', to: '/', icon: <FaStore /> },
+    { id: 2, item: 'Cart', to: '/cart', icon: <FaCartShopping /> }
+  ]
+
   return (
-    <div>
-      
-    </div>
+    <nav className='h-20 w-full bg-[#111111]'>
+      <section className='h-full w-[80%] mx-auto flex items-center justify-between'>
+        <article className='text-gray-400 flex items-center gap-3 space-y-2'>
+          <Link to='/' className='text-2xl'>
+            Shopping-Cart
+          </Link>
+          <p className=''>E-comerce</p>
+        </article>
+        <ul className='flex gap-6 text-gray-400'>
+          {paths.map(({ icon, id, item, to }) => (
+            <li key={id}>
+              <NavLink
+                className={`flex items-center gap-2 ${location.pathname === to ? 'text-orange-400' : ''
+                  }`}
+                to={to}
+              >
+                {icon}
+                {item}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </nav>
   )
 }
