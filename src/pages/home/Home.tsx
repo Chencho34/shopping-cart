fa14b5410247a533eed1fd4c441f33e85b053797
Armando <armandocrescencio343@gmail.com>
2024-04-14 22:35:45 -0600

    WIP: Add some styles in Nav, Home components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,8 +3,8 @@ import { ProductsGallery } from '../../components'
 export default function Home (): JSX.Element {
   return (
     <>
-      <article className='h-[300px] w-full flex items-center  relative bg-[url("https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-no-repeat bg-center bg-cover'>
-        <article className='w-[80%] mx-auto'>
+      <article className='h-[350px] w-full flex items-center  relative bg-[url("https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-no-repeat bg-center bg-cover'>
+        <article className='w-[80%] mt-[70px] mx-auto'>
           <div>
             <span>All Electronics Collections</span>
             <h1>Makeup Which Enhance Your Natural Bauty</h1>
