312847d88af8d6ac1068d7b5b65b2e42dfe56555
Armando <armandocrescencio343@gmail.com>
2024-04-17 06:12:18 -0600

    INSTAL: React Slick and WIP: Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,41 @@
 import { ProductsGallery } from '../../components'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+import { GrNext } from 'react-icons/gr'
+// import { GrPrevious } from 'react-icons/gr'
 
 export default function Home (): JSX.Element {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  }
   return (
     <>
-      <article className='h-[350px] w-full flex items-center  relative bg-[url("https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-no-repeat bg-center bg-cover'>
-        <article className='w-[80%] mt-[70px] mx-auto'>
-          <div>
-            <span>All Electronics Collections</span>
-            <h1>Makeup Which Enhance Your Natural Bauty</h1>
-            <button className='py-1 px-6 bg-[#212121] text-gray-300 rounded-sm'>SHOP NOW</button>
-          </div>
-        </article>
-      </article>
+      <Slider {...settings} arrows={false} className='h-full w-full object-contain rounded-b-3xl'>
+        <figure className='h-[370px] w-full overflow-hidden'>
+          <img src='https://picsum.photos/2000/1000' alt='' className='object-cover h-full w-full' />
+        </figure>
+        <figure className='h-[370px] w-full overflow-hidden'>
+          <img src='https://picsum.photos/2000/900' alt='' className='object-cover h-full w-full ' />
+        </figure>
+        <figure className='h-[370px] w-full overflow-hidden'>
+          <img src='https://picsum.photos/2000/950' alt='' className='object-cover h-full w-full ' />
+        </figure>
+        <figure className='h-[370px] w-full overflow-hidden'>
+          <img src='https://picsum.photos/2000/980' alt='' className='object-cover h-full w-full ' />
+        </figure>
+        <figure className='h-[370px] w-full overflow-hidden'>
+          <img src='https://picsum.photos/1000/980' alt='' className='object-cover h-full w-full ' />
+        </figure>
+        <figure className='h-[370px] w-full overflow-hidden'>
+          <img src='https://picsum.photos/1200/980' alt='' className='object-cover h-full w-full ' />
+        </figure>
+      </Slider>
       <ProductsGallery />      
     </>
   )
