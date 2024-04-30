d2db7e829a14cfead9d17aca10f354b68df89675
Armando <armandocrescencio343@gmail.com>
2024-04-30 14:06:10 -0600

    WIP: Responsive Design Cart Section

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -14,44 +14,58 @@ export default function Cart (): JSX.Element {
   const handleEmptyCart = () => dispatch(emptyCart())
   
   return (
-    // <div className='bg-cyan-800 h-screen w-full bg-center bg-no-repeat fixed overflow-y-auto top-0 right-0 bottom-0 left-0 bg-cover bg-[url("https://fashionista.com/.image/t_share/MTM5NDU0OTMzODExMzQwNDUy/kl1_0420jpg.jpg")]' style={{background: ''}}>
+    // <div className='h-screen w-full bg-center bg-no-repeat fixed overflow-y-auto top-0 right-0 bottom-0 left-0 bg-cover bg-[url("https://fashionista.com/.image/t_share/MTM5NDU0OTMzODExMzQwNDUy/kl1_0420jpg.jpg")]'>
       <Container className='pt-24'>
-        <h3 className='text-lg sm:text-3xl  text-[#191919] mb-10 tracking-wider'>Your Shopping Cart</h3>
-        <section className='h-full grid grid-cols-12 gap-4'>
-          <article className='col-span-8 flex flex-col gap-6'>
+        <h3 className='text-2xl sm:text-3xl text-[#191919] mb-10 tracking-wider'>Your Shopping Cart</h3>
+        <section className='h-full w-full flex justify-between flex-wrap gap-6'>
+          <article className='flex flex-col flex-1 gap-6'>
             {
               productsList.length > 0 ?
                ( productsList.map(({img, title, price, id, quantity, total}: CartProduct) => (
-                  <article key={id} className='bg-slate-100/70 backdrop-blur-sm rounded-md overflow-hidden shadow-lg hover:shadow-xl transition-shadow ease-in-out    w-full'>
-                    <section className='flex flex-row'>
-                      <figure className='w-[120px] h-[120px] object-cover'>
+                  <article key={id} className='bg-slate-100/70 backdrop-blur-sm rounded-md overflow-hidden shadow-lg hover:shadow-xl transition-shadow ease-in-out'>
+                    <section className='flex md:flex-row flex-col'>
+                      <figure className='md:w-[120px] w-full h-[120px] object-cover'>
                         <img loading='lazy' src={img} alt={title} className='h-full w-full object-cover' />
                       </figure>
-                      <article className='px-6 py-3 flex flex-row items-center justify-between w-full gap-4'>
-                        <section className='w-44'>
+                      <article className='px-3 md:px-6 py-3 flex flex-col md:flex-row items-start md:items-center justify-between w-full gap-4'>
+                        <section className='md:w-44 w-full'>
                           <h6 className='text-lg font-semibold tracking-wider'>{title}</h6>
                           <p className='text-sm tracking-wide font-semibold text-gray-800'>No. {id}</p>
                         </section>
-                        <span className='text-sm font-bold text-gray-800'>${price}</span>
-                        <section className='flex flex-row items-center gap-2'>
+                        <section className='w-full flex flex-wrap gap-0 md:gap-6 items-center justify-between'>
+                          <span className='text-sm font-bold text-gray-800'>${price}</span>
+                          <section className='flex flex-row items-center gap-2'>
+                            <button>
+                              <MdAddCircle 
+                                size={24} 
+                                className='text-[#191919] hover:text-opacity-70' 
+                                onClick={() => handleAddQuantity(id)}
+                              />
+                            </button>
+                            <span className='text-sm font-bold text-gray-800'>{quantity}</span>
+                            <button>
+                              <MdRemoveCircle 
+                                size={24} 
+                                className='text-[#191919] hover:text-opacity-70' 
+                                onClick={() => handleSubtractQuantity(id)} 
+                              />
+                            </button>
+                          </section>
+                          <span className='text-sm font-bold text-gray-800'>${total.toFixed(2)}</span>
                           <button>
-                            <MdAddCircle size={24} className='text-[#191919] hover:text-opacity-70' onClick={() => handleAddQuantity(id)} />
-                          </button>
-                          <span className='text-sm font-bold text-gray-800'>{quantity}</span>
-                          <button>
-                            <MdRemoveCircle size={24} className='text-[#191919] hover:text-opacity-70' onClick={() => handleSubtractQuantity(id)} />
+                            <MdClose
+                              size={24} 
+                              className='text-[#0706064b] hover:text-[#191919]' 
+                              onClick={() => handleRemoveFromCart(id)}
+                            />
                           </button>
                         </section>
-                        <span className='text-sm font-bold text-gray-800'>${total.toFixed(2)}</span>
-                        <button>
-                          <MdClose onClick={() => handleRemoveFromCart(id)} size={24} className='text-[#0706064b] hover:text-[#191919]' />
-                        </button>
                       </article>
                     </section>
                   </article>
                 ))
               ) : (
-                <section className='grid place-items-center h-full'>
+                <section className='h-full flex w-auto xl:py-0 py-16 justify-center items-center'>
                   <section className='grid place-items-center'>
                     <FaCartShopping size={24} color='#0706065e'/>
                     <p className='text-xl text-[#0706065e]'>Empty Cart</p>
@@ -63,13 +77,13 @@ export default function Cart (): JSX.Element {
               productsList.length > 0 && <Button className='max-w-max px-6' onClick={handleEmptyCart}>Empty Cart</Button>
             }
           </article>
-          <article className='h-min w-full col-span-4 bg-[#191919]/70 backdrop-blur-sm rounded-md p-4'>
+          <article className='h-min w-full xl:w-[370px] bg-[#191919]/70 backdrop-blur-sm rounded-md p-4'>
             <section className='flex flex-col gap-6'>
               <h3 className='text-xl md:text-3xl text-gray-300 tracking-wider'>Cart Details</h3>
               <article className='space-y-2 px-4' >
-                <p className='text-gray-300  tracking-wider'>Products: {totalCount}</p>
-                <p className='text-gray-300  tracking-wider'>Discount: $0</p>
-                <p className='text-gray-300  tracking-wider'>Subtotal: ${subTotal.toFixed(2)}</p>
+                <p className='text-gray-300 tracking-wider'>Products: {totalCount}</p>
+                <p className='text-gray-300 tracking-wider'>Discount: $0</p>
+                <p className='text-gray-300 tracking-wider'>Subtotal: ${subTotal.toFixed(2)}</p>
               </article>
               <article className='flex gap-6'>
                 <Button className='w-full'>Check Out</Button>
@@ -79,6 +93,6 @@ export default function Cart (): JSX.Element {
           </article>
         </section>
       </Container>
-    // </div>
+    // {/* </div> */}
   )
 }
