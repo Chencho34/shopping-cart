affde6435dbccd52d6f1f0222e8a11bc88d11c95
Armando <armandocrescencio343@gmail.com>
2024-06-27 11:54:42 -0600

    fix git

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { PublicLayout } from '../layout'
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<PublicLayout />}>
-      <Route index path='/' element={<Home />} />
+      {/* <Route index path='/' element={<Home />} /> */}
       <Route path='/cart' element={<Cart />} />
       <Route path='/product/:id' element={<Product />} />
       <Route path='/favorites' element={<Favorites />} />
