9fa29ca9530413eef4aa868550e39780c2742aae
Armando <armandocrescencio343@gmail.com>
2024-06-27 10:58:53 -0600

    WIP: Auth layout design

--- src/layout/authLayout/AuthLayout.tsx
+++ src/layout/authLayout/AuthLayout.tsx
@@ -2,10 +2,19 @@ import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout (): JSX.Element {
   return (
-    <>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main className='flex h-full'>
+      <aside className='w-[50%] bg-[#191919]  lg:block hidden'>
+        <article className='h-full w-full px-8 py-8 flex flex-col justify-end'>
+          <h2 className='text-slate-300 font-semibold text-6xl flex flex-col mb-9'>
+            Welcome to Shopping Cart
+          </h2>
+          <section className='text-gray-400 text-sm'>
+            <p>We're excited to have you join our community. Please take a moment to sign in or create an account to get started.</p>
+            <p>Explore a world of possibilities in fashion, tech, and more. Start your shopping journey with us today!</p>
+          </section>
+        </article>
+      </aside>
+      <Outlet />
+    </main>
   )
 }
