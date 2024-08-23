4cc0e5db2540cd261d767e3d61cd8bb6ad05474d
Armando <armandocrescencio343@gmail.com>
2024-08-23 16:23:01 -0600

    ADD: products and users services

---
+++ src/services/api/users.ts
@@ -0,0 +1,38 @@
+const API = 'http://localhost:3000'
+
+const getUsers = async () => {
+  const response = await fetch(`${API}/users`)
+  const data = await response.json()
+  return data
+}
+
+const getUserByName = async (name: string) => {
+  const response = await fetch(`${API}/user/${name}`)
+  const data = await response.json()
+  return data
+}
+
+const deleteUserByName = async (name: string) => {
+  const response = await fetch(`${API}/user/${name}`, {
+    method: 'DELETE'
+  })
+  const data = response.json()
+  return data
+}
+
+const registerUser = async (user: Object) => {
+  const response = await fetch(`${API}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+
+}
+export {
+  getUsers,
+  getUserByName,
+  deleteUserByName,
+  registerUser
+}
